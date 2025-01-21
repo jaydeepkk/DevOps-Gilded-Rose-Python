005dbe595ce589c6c508b413f061cc917e526892
Jaydeep K <jaydeep.karkare@gmail.com>
2025-01-21 15:30:10 +0000

    commit refactored code

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -5,30 +5,44 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            self.update_item_quality(item)
+            print(f"Item after update: {item}")
+
+    def update_item_quality(self, item):
+        if item.name == "Aged Brie":
+            self.update_aged_brie(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            self.update_backstage_passes(item)
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            pass  # Sulfuras doesn't change
+        else:
+            self.update_normal_item(item)
+
+    def update_aged_brie(self, item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+    def update_backstage_passes(self, item):
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+    def update_normal_item(self, item):
+        if "Conjured" in item.name:
+            degrade_rate = 2  
+        else:
+            degrade_rate = 1
+        if item.quality > 0:
+            item.quality -= degrade_rate
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= degrade_rate
--- golden_master_test.py
+++ golden_master_test.py
@@ -1,5 +1,4 @@
 import unittest
-
 from gilded_rose.gilded_rose import GildedRose
 from gilded_rose.item import Item
 
@@ -27,7 +26,7 @@ class GoldenMasterTest(unittest.TestCase):
             "[+5 Dexterity Vest, 9, 19, Aged Brie, 1, 1, Elixir of the Mongoose, 4, 6, Sulfuras, Hand of Ragnaros, 0, "
             "80, Sulfuras, Hand of Ragnaros, -1, 80, Backstage passes to a TAFKAL80ETC concert, 14, 21, "
             "Backstage passes to a TAFKAL80ETC concert, 9, 50, Backstage passes to a TAFKAL80ETC concert, 4, 50, "
-            "Conjured Mana Cake, 2, 5]"
+            "Conjured Mana Cake, 2, 4]"  # Conjured item should degrade by 2
         )
 
         for _ in range(10):
@@ -38,9 +37,8 @@ class GoldenMasterTest(unittest.TestCase):
             "[+5 Dexterity Vest, -1, 8, Aged Brie, -9, 20, Elixir of the Mongoose, -6, 0, Sulfuras, "
             "Hand of Ragnaros, 0, 80, Sulfuras, Hand of Ragnaros, -1, 80, Backstage passes to a "
             "TAFKAL80ETC concert, 4, 38, Backstage passes to a TAFKAL80ETC concert, -1, 0, "
-            "Backstage passes to a TAFKAL80ETC concert, -6, 0, Conjured Mana Cake, -8, 0]"
+            "Backstage passes to a TAFKAL80ETC concert, -6, 0, Conjured Mana Cake, -8, 0]"  # Conjured item should degrade by 4 after sell_in
         )
-
 
 if __name__ == "__main__":
     unittest.main()
